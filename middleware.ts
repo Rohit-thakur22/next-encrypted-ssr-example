8c13119cd3ea33f783eb4130a968c79bb18b8cb1
Rohit Thakur <rohitthakur@Rohits-MacBook-Air.local>
2025-10-28 00:18:33 +0530

    add middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export function middleware(request: Request) {
   const response = NextResponse.next();
-  response.headers.set("Access-Control-Allow-Origin", "https://rc8wc0o080kc4kc4ck8sok48.62.72.57.193.sslip.io");
+  response.headers.set("Access-Control-Allow-Origin", "https://nextencrypted.agileappdemo.com");
   response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   return response;
 }
--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // @ts-expect-error -- allowedDevOrigins is experimental in newer versions
-    allowedDevOrigins: [
-      "https://rc8wc0o080kc4kc4ck8sok48.62.72.57.193.sslip.io",
-    ],
-  },
+  // experimental: {
+  //   // @ts-expect-error -- allowedDevOrigins is experimental in newer versions
+  //   allowedDevOrigins: [
+  //     "https://rc8wc0o080kc4kc4ck8sok48.62.72.57.193.sslip.io",
+  //   ],
+  // },
 };
 
 export default nextConfig;
